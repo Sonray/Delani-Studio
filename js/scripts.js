bcb951fbd7236b91563a5379d53490433c0607d0
David Okwacha <osweredavid@gmail.com>
2020-07-25 15:12:05 +0300

    Togled the design on click

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-    
+
+    $("#design").click(function(){
+        $("#showDesign").toggle();
+        $("#design").toggle()
+    });
+
+    $("#showDesign").click(function(){
+        $("#design").toggle();
+        $("#showDesign").toggle()
+    });
+
 });
