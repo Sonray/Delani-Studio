56a89444b56c73207694e7e332ffc3eac59efa67
David Okwacha <osweredavid@gmail.com>
2020-07-25 18:12:42 +0300

    Added Jquery for click of the what we do container

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,26 @@ $(document).ready(function(){
         $("#showDesign").toggle()
     });
 
+    $("#development").click(function(){
+        $("#showDevelopment").toggle();
+        $("#development").toggle()
+    });
+
+    $("#showDevelopment").click(function(){
+        $("#development").toggle();
+        $("#showDevelopment").toggle()
+    });
+
+    $("#product").click(function(){
+        $("#showProduct").toggle();
+        $("#product").toggle()
+    });
+
+    $("#showProduct").click(function(){
+        $("#product").toggle();
+        $("#showProduct").toggle()
+    });
+
+    
+
 });
